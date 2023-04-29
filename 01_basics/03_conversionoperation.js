d15d89f096cb88d09cf11da17b0eadfea5de3730
Rohit433-c <131856304+Rohit433-c@users.noreply.github.com>
2023-04-29 06:32:53 +0000

    03_conversionopeeration.js

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,20 @@
+let score =" hitesh"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log( valueInNumber);
+
+
+  //note//
+//"33" =>33
+//"33abc"=> nan
+// true => 1; false=> 0
+
+let isLoggedIn = 1
+
+let booleanisloggedin = booleanisloggedin
+
